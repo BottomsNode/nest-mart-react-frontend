11410414d4fb577122a817553571d7af74a3dcf1
nishit <nishit.shivdasani@esparkbizmail.com>
2025-07-23 14:41:00 +0530

    rename the chunk name

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -9,5 +10,22 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  }
-})
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+          if (id.includes('src/pages/dashboard')) {
+            return 'dashboard';
+          }
+          if (id.includes('src/pages/auth')) {
+            return 'auth';
+          }
+        },
+      },
+    },
+  },
+});
