5eed85bfed0102f738350d02b59c8cd380332609
nishit <nishit.shivdasani@esparkbizmail.com>
2025-07-25 19:53:10 +0530

    changes in main.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+    <>
+        <App />
+        <SpeedInsights />
+    </>
+);
