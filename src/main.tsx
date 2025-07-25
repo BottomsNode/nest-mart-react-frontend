9b7a69292f0eb9f1338bc5b8acc60246c78b1b85
nishit <nishit.shivdasani@esparkbizmail.com>
2025-07-25 19:59:12 +0530

    added vercel/analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")!).render(
     <>
         <App />
         <SpeedInsights />
+        <Analytics />
     </>
 );
